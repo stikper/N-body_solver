354da93b3816e57c8d74576a61d361fb3f6f2d0f
Kirill Cherepanov <kirill2005910@gmail.com>
2025-12-06 05:06:31 +0300

    Add Simulation class

---
+++ src/Simulation.py
@@ -0,0 +1,24 @@
+from ParticleSystem import *
+from ForceCalculator import *
+from Integrator import *
+
+@dataclass
+class SimulationParameters:
+    force_calculator: ForceCalculator
+    integrator: Integrator
+    dt: float
+
+class Simulation:
+    def __init__(self, data: ParticleData, sim_params: SimulationParameters):
+        self.system = ParticleSystem(data, sim_params.force_calculator)
+        self.integrator = sim_params.integrator
+        self.dt = sim_params.dt
+        self.time = 0.0
+
+    def step(self):
+        self.integrator.step(self.system, self.dt)
+        self.time += self.dt
+
+    def get_data(self) -> ParticleData:
+        return self.system.data
+
